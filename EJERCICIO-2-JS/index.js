71cf21f7c2c3e2a6e72ff5b1eb0fc168aaae5d60
ANGEL ARIEL HERNANDEZ RESENDIZ <114241543+AngelArielGOOD@users.noreply.github.com>
2023-04-07 11:17:27 -0600

    EJERCICOO 2

---
+++ EJERCICIO-2-JS/index.js
@@ -0,0 +1,14 @@
+const misdatos = [
+    "Angel Ariel Hernandez Resendiz", 
+    17,
+    true,
+    new Date(2006, 01, 01),
+    Libro = {
+      titulo: "El principito",
+      autor: "Antoine de Saint-Exupery",
+      fecha: new Date(1943, 03, 06),
+      url: "https://www.bing.com/ck/a?!&&p=f8aa90222f5077d7JmltdHM9MTY4MDgyNTYwMCZpZ3VpZD0yNzEyMTMxOS04NmY1LTYxN2ItMjZjMi0wMTYxODdiNDYwMjEmaW5zaWQ9NTIwMw&ptn=3&hsh=3&fclid=27121319-86f5-617b-26c2-016187b46021&psq=cuento+el+principito&u=a1aHR0cDovL3dlYi5zZWR1Y29haHVpbGEuZ29iLm14L2JpYmxpb3dlYi91cGxvYWQvQW50b2luZSUyMERlJTIwU2FpbnQtRXh1cCVDMyVBOXJ5JTIwLSUyMEVsJTIwcHJpbmNpcGl0by5wZGY&ntb=1",  
+    }
+    
+ ];
+
